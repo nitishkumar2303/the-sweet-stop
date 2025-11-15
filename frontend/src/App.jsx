1b1dbbc0e497b766c22f3517f957b17fd0a4d9ca
Nitish Kumar <nk2332001@gmail.com>
2025-11-16 03:47:12 +0530

    chore(frontend): initialize React + Vite + folder structure + Tailwind base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+    <div>Let's start building frontend</div>
+    </>
+  );
+}
